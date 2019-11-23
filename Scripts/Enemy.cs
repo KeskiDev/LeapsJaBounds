e97823c71539cb0d4216b32e5529d0051c973d84
Lyle Olsen <komeaolsen@gmail.com>
2019-11-22 21:27:36 -0700

    I have no idea why enemy floats...

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -4,8 +4,11 @@ using System;
 public class Enemy : KinematicBody2D
 {
     private float gravity = 20f;
-	public float jump_force = -750f;
+	public float jump_force = -300f;
 	private Vector2 movement;
+	private float moveSpeed = 350f;
+	public float something = 0;
+	public bool jumping = false;
 	
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -14,12 +17,26 @@ public class Enemy : KinematicBody2D
     }
 	
 	public override void _PhysicsProcess(float delta){
-		if(IsOnFloor()){
-			movement.y = jump_force;
+		
+		if(IsOnWall()){
+			
+			if(Position.y <= 900f){
+				jumping = false;
+			}
+			else{
+				jumping = true;
+				GD.Print("Going up");
+				movement.y = jump_force;		
+			}
 		}
 		else{
-			movement.y = gravity;
+			if(!jumping){
+				GD.Print("Going down");
+				movement.y += gravity;
+			}
+			
 		}
+		
 		
 		movement = MoveAndSlide(movement);
 	}
